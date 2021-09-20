5bdf55c8db3483383171e2a87453f3a38e0e850c
jerrywithaz <dev@jerrywithaz.com>
2021-09-20 17:49:53 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import "reflect-metadata";
 export * from './errors';
 export * from './events';
 export * from './types/tsdocgen';
+export * from './types/docs';
+export * from './types/theme';
 export * from './models';
 export { default as getPackageJson } from './utils/getPackageJson'
 export { default } from './tsdocgen';
