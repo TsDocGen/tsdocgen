7cb04535087447541d649956ae4c23c7b49722c7
jerrywithaz <dev@jerrywithaz.com>
2021-09-21 01:56:27 -0500

    add temp build scripts

--- src/utils/isDoc.ts
+++ src/utils/isDoc.ts
@@ -2,7 +2,7 @@ import { Node, Structure } from "ts-morph";
 import { ClassDoc } from "../models";
 import AbstractDoc from "../models/documentation/AbstractDoc";
 
-export function isClassDoc(doc: AbstractDoc<Node, Structure>): doc is ClassDoc {
-    if (doc instanceof ClassDoc) return true;
+export function isClassDoc(doc: AbstractDoc<string, Node, Structure>): doc is ClassDoc {
+    if (doc.type === "class") return true;
     return false;
 }
