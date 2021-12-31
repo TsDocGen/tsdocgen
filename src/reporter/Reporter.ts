5303f87623f6b79ee17f8d050e889fbeca6636a7
jerrywithaz <dev@jerrywithaz.com>
2021-12-30 20:50:13 -0600

    update reporter

--- src/reporter/Reporter.ts
+++ src/reporter/Reporter.ts
@@ -1,28 +1,27 @@
-import { green, red, symbols } from "ansi-colors";
+import { green, red, blue, yellow } from "ansi-colors";
 
 class ReporterModel {
     info = (message: string): void => {
-        console.log(message)
+        console.log(blue('info ') + message);
     }
     verbose = (message: string): void => {
-        console.log(message)
+        console.log(message);
     }
     log = (message: string): void => {
-        console.log(message)
+        console.log(message);
     }
     success = (message: string): void => {
-        console.log(green(symbols.check + ` `) + message)
+        console.log(green('success ') + message);
     }
     error = (message: string): void => {
-        console.error(red(symbols.cross + ` `) + message)
-
+        console.error(red('error ') + message);
     }
     panic = (message: string): never => {
-        console.error(message)
-        process.exit(1)
+        console.error(message);
+        process.exit(1);
     }
     warn = (message: string): void => {
-        console.warn(message)
+        console.log(yellow('warn ') + message);
     }
 }
 
